520c939b572d90dad37b8f9e33056aec6948a3e4
Garin Prakoso <gerin.rin@gmail.com>
2020-09-25 14:17:44 +0700

    change format

--- Supra_X_125_Advance_Keyless.ino
+++ Supra_X_125_Advance_Keyless.ino
@@ -4,114 +4,43 @@
 #include <Adafruit_BMP280.h>
 #include <DS3231.h>
 #include <max6675.h>
+#include <Ticker.h>
 #include "Costum_Fonts.h"
 #include "Costum_Images.h"
-#include <Ticker.h>
-
-// WebServer Library
-// #include <ESP8266WiFi.h>
-// #include <ESPAsyncTCP.h>
-// #include <ESP8266WebServer.h>
-// #include <ESPAsyncWebServer.h>
-// #include <Hash.h>
-// #include <FS.h>
-// #include "Web_Page.h"
-
-#define SCK_PIN 14 // Pin D5 SCK=Serial CLock (Kalo di arduino Pin 13)
-#define SO_PIN 12  // Pin D6 SO=Slave Out (Kalo di arduino Pin 12)
-#define CS_PIN 15  // Pin D8 CS=Chip Select (Kalo di arduino Pin 10)
 
 DS3231 rtc;
 Adafruit_BMP280 bmp;
-// AsyncWebServer server(80);
-// WiFiEventHandler disconnectedEventHandler;
-volatile uint8_t watchDogCounter;
 Ticker secondTicker; // creating object secondTicker for WDT
-
+volatile uint8_t watchDogCounter;
+#define SCK_PIN 14							   // Pin D5 SCK=Serial CLock (Kalo di arduino Pin 13)
+#define SO_PIN 12							   // Pin D6 SO=Slave Out (Kalo di arduino Pin 12)
+#define CS_PIN 15							   // Pin D8 CS=Chip Select (Kalo di arduino Pin 10)
+MAX6675 thermocouple(SCK_PIN, CS_PIN, SO_PIN); // library baru dicoba di esp8266, gatau bisa atau engga kalau di arduino
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
-MAX6675 thermocouple(SCK_PIN, CS_PIN, SO_PIN);
 
-char *wifiSSID = "Redmiqwery1";
-char *wifiPassword = "kucing123";
-
-const uint8_t buttonPin = 16;	 //D0
-const uint8_t buzzer = 3;		 //RX
-const uint8_t primaryRelay = 13; //D7
-
-uint8_t counterOled;
-bool stateRelay = false;
-bool checkingSwitchButton, lastSwitchButton, checkingSwitchOled;
-unsigned long timeStart, millisPrintToSerial, millisDeepSleep, millisAutoTurnOff, millisOled;
+unsigned long millisOled;
 
 void setup()
 {
-	// Serial.begin(9600);
 	settingI2cDevices();
-	secondTicker.attach_ms(500, ISRWatchDog);
-	// Ketika terjadi hang, ISRWatchDog() akan berjalan setiap 0.1 detik (100ms)
-	// settingPinAndState();
-	// startWiFiAndServer();
+	settingPinAndState();
+	secondTicker.attach_ms(1000, ISRWatchDog); // Ketika terjadi hang, ISRWatchDog() akan berjalan setiap 0.1 detik (100ms)
 }
 
 void loop()
 {
-	// pressToStartTimer(buttonPin);
-	// remoteKeyless(primaryRelay, 350);
-	// autoTurnOffRelay(&stateRelay, 10000, 8, getBatteryVoltage());
-	watchDogCounter = 0; // Mereset watchDogCounter menjadi 0
-	switchAndDisplayOledScreen(stateRelay, 200, 70);
-	// printToSerial(30);
-	// deepSleepMode(stateRelay, 5000);
+	watchDogCounter = 0;	 // Mereset watchDogCounter menjadi 0
+	displayTimeAndDate(300); // OLED merefresh layar setiap 0.3 detik sekali
 }
 
 void ISRWatchDog()
 {
 	watchDogCounter++;		  // Ketika terjadi hang, akan terus menambah watchDogCounter
-	if (watchDogCounter == 5) // Jika watchDogCounter sudah mencapai 5
+	if (watchDogCounter == 7) // Jika watchDogCounter sudah mencapai 5
 	{
 		ESP.reset(); // Akan melakukan reset
 	}
 }
-
-// void startWiFiAndServer()
-// {
-// 	WiFi.begin(wifiSSID, wifiPassword);
-
-// 	disconnectedEventHandler = WiFi.onStationModeDisconnected([](const WiFiEventStationModeDisconnected &event) {
-// 		Serial.println("Station disconnected, trying to reconnect");
-// 	});
-
-// 	if (!SPIFFS.begin())
-// 	{
-// 		Serial.println("An Error has occurred while mounting SPIFFS");
-// 	}
-
-// 	File file = SPIFFS.open("/index.html", "r");
-// 	if (!file)
-// 	{
-// 		Serial.println("Failed to open file for reading");
-// 	}
-
-// 	file.close();
-
-// 	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-// 		request->send(SPIFFS, "/index.html");
-// 	});
-
-// 	server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request) {
-// 		request->send_P(200, "text/plain", String(bmp.readTemperature()).c_str());
-// 	});
-
-// 	server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request) {
-// 		request->send_P(200, "text/plain", String(bmp.readAltitude(1013.25)).c_str());
-// 	});
-
-// 	server.on("/pressure", HTTP_GET, [](AsyncWebServerRequest *request) {
-// 		request->send_P(200, "text/plain", String(getBatteryVoltage()).c_str());
-// 	});
-
-// 	server.begin();
-// }
 
 void settingI2cDevices()
 {
@@ -120,239 +49,81 @@ void settingI2cDevices()
 	display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 	display.setTextColor(WHITE);
 	display.clearDisplay();
+	display.drawBitmap(0, 0, supraX125Bitmap, 128, 64, WHITE);
 	display.display();
+	delay(1500);
 }
 
 void settingPinAndState()
 {
-	digitalWrite(primaryRelay, HIGH);
-	digitalWrite(buzzer, HIGH);
-
-	pinMode(buttonPin, INPUT_PULLUP);
-	pinMode(buzzer, OUTPUT);
-	pinMode(primaryRelay, OUTPUT);
 }
 
-void printToSerial(int duration)
+void displayTimeAndDate(int refreshInterval)
 {
-	if (millis() - millisPrintToSerial >= duration)
+	if (millis() - millisOled >= refreshInterval)
 	{
-		millisPrintToSerial = millis();
-		Serial.print(" batteryVoltage   : ");
-		Serial.print(getBatteryVoltage());
-		Serial.print("   millis() - timeStart   : ");
-		Serial.print(millis() - timeStart);
-		Serial.print("   counterOled   : ");
-		Serial.print(counterOled);
-		Serial.println("");
-	}
-}
-
-void remoteKeyless(byte inputRelay, int pressDuration)
-{
-	if ((millis() - timeStart) >= pressDuration && checkingSwitchButton)
-	{
-		if (stateRelay == LOW)
-		{
-			stateRelay = HIGH; // Mematikan RELAY
-			turnOnBuzzer(1, 200);
-		}
-		else
-		{
-			stateRelay = LOW; // Nyalakan RELAY
-			turnOnBuzzer(2, 200);
-		}
-		checkingSwitchButton = false;
-		digitalWrite(inputRelay, stateRelay);
-	}
-}
-
-void pressToStartTimer(byte inputButton)
-{
-	bool currentSwitchButton = digitalRead(inputButton);
-	if (currentSwitchButton != lastSwitchButton)
-	{
-		if (currentSwitchButton == LOW)
-		{
-			timeStart = millis();
-			checkingSwitchButton = true;
-			checkingSwitchOled = true;
-		}
-		else
-		{
-			checkingSwitchButton = false;
-			checkingSwitchOled = false;
-		}
-		lastSwitchButton = currentSwitchButton;
-	}
-}
-
-void deepSleepMode(bool relayCondition, int durBeforeSleep)
-{
-	if (relayCondition == true)
-	{
-		if (millis() - millisDeepSleep >= durBeforeSleep)
-		{
-			millisDeepSleep = millis();
-			ESP.deepSleep(0); // deep sleep permanen
-		}
-	}
-}
-
-void autoTurnOffRelay(bool *relayCondition, int durBeforeTurnOff, byte voltThreshold, float batteryVoltage)
-{
-	if (*relayCondition == true)
-	{
-		millisAutoTurnOff = millis();
-		return;
-	}
-
-	if (batteryVoltage >= voltThreshold)
-	{
-		millisAutoTurnOff = millis();
-		return;
-	}
-
-	if (batteryVoltage <= voltThreshold && millis() - millisAutoTurnOff >= durBeforeTurnOff)
-	{
-		*relayCondition = true;
-		turnOnBuzzer(1, 200);
-		digitalWrite(primaryRelay, *relayCondition);
-		return;
-	}
-}
-
-void turnOnBuzzer(byte times, int duration)
-{
-	for (int i = 1; i <= times; i++)
-	{
-		digitalWrite(buzzer, false);
-		delay(duration);
-		digitalWrite(buzzer, true);
-		delay(duration);
-	}
-}
-
-float getBatteryVoltage()
-{
-	float vOUT, vIN;
-	uint16_t analogValue;
-	float R1 = 30000;
-	float R2 = 7500;
-
-	// @ts-ignore
-	analogValue = analogRead(A0);
-	vOUT = (analogValue * 3.3) / 1024;
-	return vIN = vOUT / (R2 / (R1 + R2));
-}
-
-void switchAndDisplayOledScreen(bool relayCondition, int refreshDuration, uint8_t pressDuration)
-{
-	if (relayCondition == true)
-	{
-		Wire.endTransmission();
-		display.clearDisplay();
-		display.display();
-		return;
-	}
-
-	if ((millis() - timeStart) >= pressDuration && checkingSwitchOled)
-	{
-		counterOled++;
-		checkingSwitchOled = false;
-	}
-
-	if (millis() - millisOled >= refreshDuration)
-	{
-		Wire.endTransmission(false);
 		millisOled = millis();
-		switch (counterOled)
+
+		bool h12 = false;
+		bool PM = false;
+		bool Century = false;
+		byte nowHour = rtc.getHour(h12, PM);
+		int8_t tinggiDisplay = -3;
+		display.clearDisplay();
+
+		display.setFont(&Cousine_Bold_11);
+		display.setCursor(3, 16 + tinggiDisplay); // atur posisi kalender
+
+		display.print(rtc.getDate());
+		display.print('-');
+		display.print(stringOfMonth(rtc.getMonth(Century)));
+		display.print('-');
+		display.print(rtc.getYear());
+
+		display.setFont(&DSEG7_Classic_Bold_21);
+
+		display.setCursor(6, 43 + tinggiDisplay);
+		if (nowHour < 10)
+			display.print('0');
+		display.print(nowHour);
+		display.print(':');
+
+		display.setCursor(47, 43 + tinggiDisplay);
+		if (rtc.getMinute() < 10)
+			display.print('0');
+		display.print(rtc.getMinute());
+		display.print(':');
+
+		display.setCursor(88, 43 + tinggiDisplay);
+		if (rtc.getSecond() < 10)
+			display.print('0');
+		display.print(rtc.getSecond());
+
+		display.drawLine(3, 50 + tinggiDisplay, 125, 50 + tinggiDisplay, WHITE); // print garis bawah
+
+		if (bmp.readAltitude(1013.25) < 100)
 		{
-		case 1:
-			displayTimeAndDate(bmp.readTemperature());
-			break;
-		case 2:
-			displayAltitude(bmp.readAltitude(1013.25));
-			break;
-		case 3:
-			displayBatteryVoltage(getBatteryVoltage());
-			break;
-		case 4:
-			displayEngineTemperature(thermocouple.readCelsius());
-			break;
-		case 5:
-			displayTachometer(7500);
-			break;
-		// case 6:
-		// 	displayWifiConnectifity();
-		// 	break;
-		default:
-			counterOled = 1;
-			break;
+			display.setCursor(85, 62 + tinggiDisplay); // setting posisi tampilan MDPL
 		}
+		else
+		{
+			display.setCursor(73, 62 + tinggiDisplay); // setting posisi tampilan MDPL
+		}
+
+		display.setFont();
+		display.setTextSize(1);
+		display.print(bmp.readAltitude(1013.25), 0); // baca ketinggian
+		display.print(" MDPL");						 // print tulisan MDPL
+
+		display.setCursor(89, 9 + tinggiDisplay); // setting posisi hari
+		display.print(DayOfWeek(rtc.getDoW()));	  // baca hari
+
+		display.setCursor(7, 56 + tinggiDisplay);  // setting posisi suhu mesin
+		display.print(thermocouple.readCelsius()); // tampilkan suhu mesin ke OLED
+		display.print("C");						   // print huruf C
+
+		display.display(); // tampilkan ke OLED
 	}
-}
-
-void displayTimeAndDate(float getTemperature)
-{
-	bool h12 = false;
-	bool PM = false;
-	bool Century = false;
-	byte nowHour = rtc.getHour(h12, PM);
-	int8_t tinggiDisplay = -3;
-	display.clearDisplay();
-
-	display.setFont(&Cousine_Bold_11);
-	display.setCursor(32, 16 + tinggiDisplay);
-
-	display.print(rtc.getDate());
-	display.print('-');
-	display.print(stringOfMonth(rtc.getMonth(Century)));
-	display.print('-');
-	display.print(rtc.getYear());
-
-	display.setFont(&DSEG7_Classic_Bold_21);
-
-	display.setCursor(6, 43 + tinggiDisplay);
-	if (nowHour < 10)
-		display.print('0');
-	display.print(nowHour);
-	display.print(':');
-
-	display.setCursor(47, 43 + tinggiDisplay);
-	if (rtc.getMinute() < 10)
-		display.print('0');
-	display.print(rtc.getMinute());
-	display.print(':');
-
-	display.setCursor(88, 43 + tinggiDisplay);
-	if (rtc.getSecond() < 10)
-		display.print('0');
-	display.print(rtc.getSecond());
-
-	display.drawLine(3, 50 + tinggiDisplay, 125, 50 + tinggiDisplay, WHITE);
-
-	display.setFont(&Meteocons_Regular_11);
-	display.setCursor(80, 64 + tinggiDisplay);
-	if (nowHour > 18 || nowHour < 6)
-	{
-		display.print('C');
-	}
-	else
-	{
-		display.print('B');
-	}
-
-	display.setCursor(93, 62 + tinggiDisplay);
-	display.setFont();
-	display.setTextSize(1);
-	display.print(getTemperature, 1);
-	display.print("C");
-
-	display.setCursor(7, 56 + tinggiDisplay);
-	display.print(DayOfWeek(rtc.getDoW()));
-
-	display.display();
 }
 
 String DayOfWeek(uint8_t dayOfWeek)
@@ -410,146 +181,3 @@ String stringOfMonth(uint8_t getMonth)
 		break;
 	}
 }
-
-void displayAltitude(float getAltitude)
-{
-	display.clearDisplay();
-	display.drawBitmap(0, 19, MountainBitmap2, 50, 50, WHITE);
-
-	display.setFont(&Cousine_Bold_11);
-	display.setCursor(50, 17);
-	display.print("Altitude");
-
-	display.setCursor(85, 55);
-	display.print("MDPL");
-
-	display.setFont(&DSEG7_Classic_Bold_21);
-	display.setCursor(47, 44);
-	if (getAltitude < 1000)
-		display.print('0');
-	if (getAltitude < 100)
-		display.print('0');
-	if (getAltitude < 10)
-		display.print('0');
-	display.print(getAltitude, 0);
-
-	display.display();
-}
-
-void displayBatteryVoltage(float getVoltage)
-{
-	display.clearDisplay();
-	display.drawBitmap(8, 18, BatteryBitmap, 40, 40, WHITE);
-
-	display.setFont(&Cousine_Bold_11);
-	display.setCursor(58, 20);
-	display.print("Battery");
-
-	display.setFont(&DSEG7_Classic_Bold_26);
-	display.setCursor(44, 54);
-	if (getVoltage < 10)
-	{
-		display.setCursor(56, 54);
-	}
-	display.print(getVoltage, 1);
-
-	display.setFont(&Cousine_Bold_11);
-	display.print("V");
-
-	display.display();
-}
-
-void displayEngineTemperature(float getEngineTemp)
-{
-	display.clearDisplay();
-	display.setTextSize(1);
-	display.drawBitmap(8, 17, EngineTempBitmap, 40, 40, WHITE);
-
-	display.setFont(&Cousine_Bold_11);
-	display.setCursor(55, 20);
-	display.print("Engine");
-
-	display.setFont(&DSEG7_Classic_Bold_26);
-	display.setCursor(53, 54);
-	if (getEngineTemp > 100)
-	{
-		display.setCursor(41, 54);
-	}
-	display.print(getEngineTemp, 0);
-
-	display.setCursor(99, 46);
-	if (getEngineTemp > 100)
-	{
-		display.setCursor(109, 46);
-	}
-	display.setFont();
-	display.setTextSize(2);
-	display.print('C');
-	display.setTextSize(1);
-
-	display.display();
-}
-
-void displayTachometer(uint16_t getTachometer)
-{
-	display.clearDisplay();
-
-	int tachometerLevel = map(getTachometer, 0, 8000, 64, 35);
-	if (tachometerLevel < 35)
-	{
-		tachometerLevel = 35;
-	}
-	display.fillRect(119, tachometerLevel, 9, 64, WHITE);
-	display.fillRect(0, tachometerLevel, 9, 64, WHITE);
-
-	display.setFont(&Cousine_Bold_11);
-	display.setCursor(26, 14);
-	display.print("Tachometer");
-
-	display.setFont(&DSEG7_Classic_Bold_26);
-	display.setCursor(21, 48);
-
-	if (getTachometer < 1000)
-		display.print('0');
-	if (getTachometer < 100)
-		display.print('0');
-	if (getTachometer < 10)
-		display.print('0');
-	display.print(getTachometer);
-
-	display.setFont(&Cousine_Bold_11);
-	display.setCursor(54, 60);
-	display.print("RPM");
-
-	display.drawLine(0, 35, 17, 35, WHITE);
-	display.drawLine(112, 35, 128, 35, WHITE);
-
-	display.display();
-}
-
-// void displayWifiConnectifity()
-// {
-// 	display.clearDisplay();
-// 	display.drawBitmap(1, 9, wifiBitmap, 40, 40, WHITE);
-
-// 	display.setFont(&Cousine_Bold_11);
-// 	display.setCursor(6, 57);
-// 	display.print("WiFi");
-// 	display.setCursor(58, 20);
-// 	display.print("Status");
-
-// 	display.setCursor(42, 38);
-// 	display.setFont();
-// 	display.setTextSize(1);
-// 	if (WiFi.status() == WL_CONNECTED)
-// 	{
-// 		display.print(WiFi.localIP());
-// 	}
-// 	else
-// 	{
-// 		display.setCursor(48, 34);
-// 		display.print("Reconnecting");
-// 	}
-
-// 	display.display();
-// }
